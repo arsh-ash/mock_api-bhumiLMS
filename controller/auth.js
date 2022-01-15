9671fe7ec17b8284e68817a3ebbe35795aa1f7e4
Mohd Arsh <70385742+arsh-ash@users.noreply.github.com>
2022-01-15 17:38:39 +0530

    adedd passport-jwt strategy and login route

--- controller/auth.js
+++ controller/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/users");
+const jwt=require("jsonwebtoken");
 
 //@desc    create User
 //@route   POST /api/v1/auth/users
@@ -22,4 +23,36 @@ exports.register = async (req, res, next) => {
             message: `${err}`
         })
     }
+}
+
+
+exports.login=async function(req,res){
+    console.log("arshhhhhhh",req.body.email);
+    
+    try{
+        let user = await User.findOne({email: req.body.email});
+        console.log("zzzzzzz",user);
+    
+
+        if (!user || user.password != req.body.password){
+            return res.json(422, {
+                message: "Invalid username or password"
+            });
+        }
+
+        return res.json(200, {
+            message: 'Sign in successful, here is your token, please keep it safe!',
+            data:  {
+                token: jwt.sign(user.toJSON(), 'Bhumi', {expiresIn:  '100000000'})
+            }
+        })
+
+    }catch(err){
+        console.log('********', err);
+        return res.json(500, {
+            message: "Internal Server Error"
+        });
+    }
+
+
 }
--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const connectDB=require('./config/database')
 
 
 connectDB();
+const passport=require("passport");
 
-app.use(express.json()); // it will allow us to use req.body
+
+const passportJWT=require("./config/passport-jwt-strategy");
+
+
+app.use(express.json());
+app.use(passport.initialize());
+; // it will allow us to use req.body
+
+
 app.use("/",require("./routes"));
 app.listen(port,function(err){
     if(err){
