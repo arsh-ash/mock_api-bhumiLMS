ef611d22261ebad1b325f493a0873e95a1ce4039
hem-antsy <theman@hemantsingh.in>
2022-01-18 01:43:11 +0530

    added semi-colon & accepting user email in lowercase

--- models/courseCategory.js
+++ models/courseCategory.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-
   category: { type: String, required: [true, "Please add Category"] },
-
-  
 });
 
 module.exports = mongoose.model("Category", categorySchema);
--- models/users.js
+++ models/users.js
@@ -1,33 +1,34 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, "Please add Name"],
-    },
-    lastName: {
-        type: String,
-    },
-    email: {
-        type: String,
-        required: [true, "Please enter email address"],
-        unique: true,
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            "Please add valid email address",
-        ],
-    },
-    role: {
-        type: String,
-        enum: ["admin", "instructor","student"],
-        default: "student",
-    },
-    password: {
-        type: String,
-        required: [true, "Please add a password"],
-        minlength: 6,
-        // select: false, // it will not show the password when we fetch the user details
-    },
+  firstName: {
+    type: String,
+    required: [true, "Please add your Name"],
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email address"],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please add a valid email address",
+    ],
+    lowercase: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "instructor", "student"],
+    default: "student",
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password. Minimum length 6 characters"],
+    minlength: 6,
+    // select: false, // it will not show the password when we fetch the user details
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
