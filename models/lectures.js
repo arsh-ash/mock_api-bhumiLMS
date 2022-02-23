c0ea641154f9b9c5282675158b7cacae3dce61a1
Mohd Arsh <70385742+arsh-ash@users.noreply.github.com>
2022-02-23 11:32:49 +0530

    created video upload api for course content

--- models/lectures.js
+++ models/lectures.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const path=require("path");
+const multer=require("multer");
 const VIDEOS_PATH = path.join("/uploads/lectures/videos");
 
 const lectureSchema = new Schema(
@@ -31,7 +33,7 @@ const lectureSchema = new Schema(
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     console.log("File fetched", file);
-    cb(null, path.join(__dirname, "..", IMAGES_PATH));
+    cb(null, path.join(__dirname, "..", VIDEOS_PATH));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -40,9 +42,9 @@ const storage = multer.diskStorage({
   },
 });
 
-UserSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+lectureSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
   "avatar"
 );
-UserSchema.statics.avatarPath = VIDEOS_PATH;
+lectureSchema.statics.avatarPath = VIDEOS_PATH;
 
 module.exports = mongoose.model("Lectures", lectureSchema);
